27cb3e698a19437a4458e31761f33391d2f9f5c1
Mira <323581089+MiraEclipse@users.noreply.github.com>
2026-09-13 17:19:16 +0200

    v1

--- src/Kontor.App/obj/Debug/net8.0-windows/Kontor.App.AssemblyInfo.cs
+++ src/Kontor.App/obj/Debug/net8.0-windows/Kontor.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kontor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf2b44995b66a8170bd17ce7051f821d8e0f065")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+777d614bb0e5c450b7a5fdff23aca4ea93c70516")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kontor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kontor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
